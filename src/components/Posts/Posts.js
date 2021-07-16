171fc0739ad37c69570b15f9a0c0365a99e0968d
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2021-07-16 17:16:41 +0600

    fixed skeletonjs

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Grid  } from "@material-ui/core";
 import { useSelector } from "react-redux";
 import SinglePostSkeleton from '../../Skeleton/SinglePostSkeleton/SinglePostSkeleton';
@@ -7,19 +7,27 @@ import Post from "./Post/Post";
 import useStyles from "./styles";
 
 const Posts = ({ setCurrentId }) => {
+  const [timer, setTimer] = useState(true);
   const { posts, isLoading } = useSelector((state) => state.posts);
   console.log(JSON.stringify(posts));
   const classes = useStyles();
-  if (!posts.length && !isLoading) return 'No posts'
-  return (isLoading ?
 
+  useEffect(() => {
+    setTimeout(() => {
+      setTimer(false);
+    }, 4000);
+  }, []);
+
+  if (!posts.length && !isLoading) return 'No posts';
+
+  return  (isLoading && timer ) || ( !isLoading && timer) ? (
     <div style={{ display: 'flex', flexWrap:'wrap' }}>
       {[1, 2, 3, 4, 5, 6, 7, 8].map((loading) => (
       <div style={{width:'200px', padding:'10px', marginBottom:'10px'}}>
         <SinglePostSkeleton />
       </div>
       ))}
-    </div>
+    </div>)
 
     :
     
